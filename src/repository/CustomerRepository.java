0f1dc779f72932d207d870b0950c87d987f4f3b5
namankaushik <kaushiknaman68@gmail.com>
2025-12-29 23:22:56 +0530

    created CustomerRepository

--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -3,6 +3,7 @@ package repository;
 import domain.Customer;
 import domain.Transaction;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,4 +11,7 @@ import java.util.Map;
 public class CustomerRepository {
     private final Map<String , Customer> customersById = new HashMap<>();
 
+    public List<Customer> findALl() {
+        return new ArrayList<>(customersById.values());
+    }
 }
