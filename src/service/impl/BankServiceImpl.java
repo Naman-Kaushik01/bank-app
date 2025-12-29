4ff2a9ed57ca78b63f61d947291bd87f7644ae4a
namankaushik <kaushiknaman68@gmail.com>
2025-12-29 14:08:53 +0530

    getAccountNumber

--- src/service/impl/BankServiceImpl.java
+++ src/service/impl/BankServiceImpl.java
@@ -1,22 +1,31 @@
 package service.impl;
 
 import domain.Account;
+import repository.AccountRepository;
 import service.BankService;
 
 import java.util.UUID;
 
 public class BankServiceImpl implements BankService {
 
+    private final AccountRepository accountRepository = new AccountRepository();
     @Override
     public String openAccount(String name, String email, String accountType) {
         String customerId = UUID.randomUUID().toString();
 
         // CHANGE LATER --> 10 + 1 = AC11
-        String accountNumber = UUID.randomUUID().toString();
+//        String accountNumber = UUID.randomUUID().toString();
+        String accountNumber = getAccountNumber();
         Account account = new Account(accountNumber , accountType ,0,customerId);
 
         //SAVE
+        accountRepository.saveAccount(account);
+        return accountNumber;
 
-        return "";
+    }
+
+    private String getAccountNumber() {
+        int size = accountRepository.findAll().size() + 1;
+        return String.format("AC%06D" , size);
     }
 }
