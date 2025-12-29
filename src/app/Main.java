5a39f1f79100363927b1ad0bb6d6848bb7e64d1e
namankaushik <kaushiknaman68@gmail.com>
2025-12-29 14:12:44 +0530

    created listAccount

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,14 @@
 package app;
 
+import service.BankService;
+import service.impl.BankServiceImpl;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        BankService bankService = new BankServiceImpl();
         boolean running = true;
         System.out.println("Welcome to Console Bank");
         while (running) {
@@ -23,7 +27,7 @@ public class Main {
             System.out.println("Your choice is " + choice);
 
             switch (choice) {
-                case "1" -> openAccount(sc);
+                case "1" -> openAccount(sc , bankService);
                 case "2" -> deposit(sc);
                 case "3" -> withdraw(sc);
                 case "4" -> transfer(sc);
@@ -37,7 +41,7 @@ public class Main {
 
     }
 
-    private static void openAccount(Scanner sc) {
+    private static void openAccount(Scanner sc, BankService bankService) {
         System.out.println("Customer Name : ");
         String name = sc.nextLine().trim();
         System.out.println("Customer Email : ");
@@ -47,6 +51,7 @@ public class Main {
         System.out.println("Initial deposit (optional , blank for 0) ");
         String amountStr = sc.nextLine().trim();
         Double initial = Double.valueOf(amountStr);
+        bankService.openAccount(name, email, type);
     }
 
     private static void deposit(Scanner sc) {
@@ -59,6 +64,8 @@ public class Main {
     }
 
     private static void statement(Scanner sc) {
+    }
+    private static void listAccounts(Scanner sc) {
     }
 
     private static void searchAccount(Scanner sc) {
